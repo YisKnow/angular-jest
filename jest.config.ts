924db94c7cbc6db78c5cc0b8ee24c15f6959a257
Jesús Alejandro Atao <jealeat96@gmail.com>
2025-08-20 22:51:58 -0500

    Jest config

---
+++ jest.config.ts
@@ -0,0 +1,19 @@
+import type { Config } from "jest";
+
+export default {
+  preset: "jest-preset-angular",
+  moduleDirectories: ["node_modules", "<rootDir>"],
+  testMatch: ["**/+(*.)+(spec).+(ts)"],
+  moduleNameMapper: {
+    "@shared/(.*)": "<rootDir>/src/app/domains/shared/$1",
+    "@products/(.*)": "<rootDir>/src/app/domains/products/$1",
+    "@info/(.*)": "<rootDir>/src/app/domains/info/$1",
+    "@env/(.*)": "<rootDir>/src/environments/$1",
+  },
+  coverageDirectory: "./coverage",
+  collectCoverageFrom: [
+    "src/app/**/*.ts",
+    "!<rootDir>/node_modules/",
+    "!<rootDir>/test/",
+  ],
+} satisfies Config;
